37b4720467bc5951151d65818786d66e82ed3799
Islam NEGM <islam.negm@valeo.com>
2021-12-08 16:19:43 +0200

    Implement gcd

--- rsa/rsa.py
+++ rsa/rsa.py
@@ -1,29 +1,56 @@
+
+import random
+
+'''
+  Helper Functions
+'''
+def get_rand_bits(bits):
+  return random.getrandbits(bits)
+
 '''
   Random Prime Numbers
 '''
-
 # 1. Primality Test
-# 1.a. Euclidian GCD
+# 1.a. Euclidian GCD (m, n)
 
 def euclid_gcd(m, n):
   #
-  # @ref : https://djangocentral.com/gcd-using-euclid-algorithm-in-python/
-  # 
-  # Assuming m = n*q + r ; q: quotient and r: reminder
+  # @ref : Slide 15 CIT 620
   #
-  # Rearrange the m,n if n > m
-  temp = 0
-  if (n > m):
-    temp = n
-    n = m
-    m = temp
-  r = m % n
-  while(r != 0):
-      m = n
-      n = r
-      q = int(m / n)
-      r = m % n
-  return r
+  # Assume the equation
+  #  m_lst = div * quotient_lst + remainder_lst; m > n
+
+  # Sanity check m > n
+  if (n < m):
+    temp = m
+    m = n
+    n = temp
+
+  # Using arrays as a way to hold the division steps
+  remainder_lst = []
+  quotient_lst = []
+  m_lst = []
+
+  # For the first time the remainder_lst = n (second smaller number)
+  m_lst.append(m)
+  remainder_lst.append(m_lst[0] % n)
+  quotient_lst.append(n)                          # In next rounds it will be remainder[i-1]
+  div = m_lst[0] // quotient_lst[0]
+
+  i = 0
+  while (remainder_lst[i] != 0):                     # Continue until remainder = 0
+    # Each round
+    i = i+1
+    # 1.a. Shift previous step remainder_lst into current step quotient_lst
+    quotient_lst.append(remainder_lst[i-1])
+    # 1.b. Shift previous step quotient_lst into current m_lst
+    m_lst.append(quotient_lst[i-1])
+    # 2. Calculate current div
+    div = m_lst[i] // quotient_lst[i]
+    # 3. Calculate current remainder_lst
+    remainder_lst.append(m_lst[i] % quotient_lst[i])  
+  return remainder_lst[i-1]
+  
 
 def calc_n(p, q):
   return p*q
--- rsa/test_key-generation.py
+++ rsa/test_key-generation.py
@@ -11,3 +11,32 @@ def test_calc_n__it_generates_n__equals_pq():
   PRIME_1 = 61
   PRIME_2 = 53
   assert rsa.calc_n(PRIME_1, PRIME_2) == 3233
+
+def test_get_rand_bits__it_generates_not_equal_numbers():
+  # Arrange
+  rand1 = rsa.get_rand_bits(1024)
+  # Act
+  rand2 = rsa.get_rand_bits(1024)
+  # Assert
+  assert rand1 != rand2
+
+def test_euclid_gcd__it_finds_eculid_gcd():
+  # Arrange
+  expected = 17
+  m = 323
+  n = 238
+  # Act
+  actual = rsa.euclid_gcd(m,n)
+  # Assert
+  assert actual == expected
+
+def test_euclid_gcd__it_finds_eculid_gcd_for_rearranged_parameters():
+  # Arrange
+  expected = 17
+  m = 238
+  n = 323
+  # Act
+  actual = rsa.euclid_gcd(m,n)
+  # Assert
+  assert actual == expected
+
